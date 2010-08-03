2bcd7160f79927b999355ad69a8cf501d43113f7
mobileAgent <flester@gmail.com>
2010-08-03 18:31:16 -0400

    basic speakers index

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,7 +1,17 @@
 class SpeakersController < ApplicationController
 
   def index
-    @speakers = Speaker.all(:order => :last_name)
+    @speakers = Speaker.all(:order => 'last_name,first_name')
+    @message_counts = AudioMessage.active.count(:group => :speaker_id)
   end
-  
+
+  def show
+    @speaker = Speaker.find(params[:id])
+    @query_title = "Messages by #{@speaker.full_name}"
+    @items = AudioMessage.active.paginate(:page => params[:page],
+                                            :conditions => ["speaker_id = ?",@speaker.id],
+                                            :order => 'msg,subj')
+    render :template => 'welcome/index'
+  end
+
 end
