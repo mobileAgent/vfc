a597bf8917f45b87ce9ba2c46da4f0646e357803
mobileAgent <flester@gmail.com>
2012-04-23 22:41:41 -0400

    remove sorting links from motm page, add width on date col

--- app/controllers/motms_controller.rb
+++ app/controllers/motms_controller.rb
@@ -1,7 +1,10 @@
 class MotmsController < ApplicationController
 
   def index
-    @motms = Motm.find(:all, :order => 'created_at desc')
+    @motm = Rails.cache.fetch('motm',:expires => 30.minutes) {
+      Motm.active.last
+    }
+    @motms = Motm.find(:all, :order => "created_at desc")
   end
   
 end
