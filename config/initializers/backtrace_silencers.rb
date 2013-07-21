1f308cafd0dc5d581e2edc8f97b39db969270c1c
mobileAgent <flester@gmail.com>
2013-07-20 21:43:13 -0400

    silence partial render log messages in production

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,15 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+module ActionView
+  class LogSubscriber
+    def render_template(event)
+      message = "Rendered #{from_rails_root(event.payload[:identifier])}"
+      message << " within #{from_rails_root(event.payload[:layout])}" if event.payload[:layout]
+      message << (" (%.1fms)" % event.duration)
+      debug(message)
+    end
+    alias :render_partial :render_template
+    alias :render_collection :render_template
+  end  
+end  
