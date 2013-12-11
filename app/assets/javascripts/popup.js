bf37a14f024c914835f453d276e84d9275da1aa2
mobileAgent <flester@gmail.com>
2013-12-10 22:28:12 -0500

    fix mp3 download behavior to not leave the current page

---
+++ app/assets/javascripts/popup.js
@@ -0,0 +1,12 @@
+function dl(el) {
+    var hiddenIFrameID = 'hiddenDownloader',
+        iframe = document.getElementById(hiddenIFrameID);
+    if (iframe === null) {
+        iframe = document.createElement('iframe');
+        iframe.id = hiddenIFrameID;
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+    }
+    iframe.src = $(el).attr('href');
+    return false;
+}
