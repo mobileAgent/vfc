3e29b9df3b5a0e7a7427a703734e34a0ac938583
mobileAgent <flester@gmail.com>
2014-02-04 22:19:47 -0500

    catch exception from new version of ts on pages that we aren't even generating links to

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,22 +1,25 @@
 class TagsController < ApplicationController
 
   def show
-    @query_title = t(:query_by_tag, :tagname => params[:id])
-    @page_title = params[:id]
-    @items = AudioMessage.search('',
-                :conditions => {:tags => params[:id]},
-                :page => params[:page] || 1,
-                :per_page => AudioMessage.per_page,
-                :order => sort_column,
-                :include => [:language, :speaker, :place, :tags])
-    logger.debug "Found #{@items.size} items"
-
-    if request.post? && params[:download] && download_zipline(@items,@query_title,params[:page])
-      return
-    else
+    begin
+      @query_title = t(:query_by_tag, :tagname => params[:id])
+      @page_title = params[:id]
+      @items = AudioMessage.search('',
+                                   :conditions => {:tags => params[:id]},
+                                   :page => params[:page] || 1,
+                                   :per_page => AudioMessage.per_page,
+                                   :order => sort_column,
+                                   :include => [:language, :speaker, :place, :tags])
+      logger.debug "Found #{@items.size} items"
+      
+      if request.post? && params[:download] && download_zipline(@items,@query_title,params[:page])
+        return
+      else
+        render :template => 'welcome/index'
+      end
+    rescue Riddle::OutOfBoundsError
       render :template => 'welcome/index'
     end
-
   end
   
 end
